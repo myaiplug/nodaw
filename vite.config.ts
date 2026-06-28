6488d7b531bc7a96ec9605057fa7565b43b0275d
myaiplug <myaiplug@users.noreply.github.com>
2026-06-28 04:59:06 -0400

    Fix base URL for GH Pages, remove duplicate AdSense init

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,13 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isGHPages = process.env.GITHUB_ACTIONS === 'true';
+const base = isGHPages ? '/nodaw/' : '/';
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base,
       server: {
         port: 3000,
         host: '0.0.0.0',
